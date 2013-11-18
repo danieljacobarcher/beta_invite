2ebfeb35b26f0a1eb08e479d0000f9f4f1b005b4
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-11-18 10:39:05 -0800

    Removed attr_accessible

--- app/models/beta_invite/beta_invite.rb
+++ app/models/beta_invite/beta_invite.rb
@@ -1,6 +1,5 @@
 module BetaInvite
   class BetaInvite < ActiveRecord::Base
-    attr_accessible :email, :token
   
     # validating email with presence, uniqueness and format 
     validates :email, presence: true, uniqueness: true, format: { with: BetaInviteSetup.email_validation_regex }
